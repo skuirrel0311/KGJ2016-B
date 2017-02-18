06157aa9799c800915abb74a4b3abdee2c3608e5
GCR-B06\ladmin <g1427050@st.t-kougei.ac.jp>
2017-02-18 11:33:54 +0900

    持ち込んだアセットを導入

---
+++ KGJ2016-B/Assets/Programer/GameManager/AudioManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : BaseManager<AudioManager>
+{
+    AudioSource audioSource;
+    AudioSource loopAudioSource;
+
+    [SerializeField]
+    List<AudioClip> clipList = new List<AudioClip>();
+
+    void Start()
+    {
+        AudioSource[] temp = GetComponents<AudioSource>();
+
+        audioSource = temp[0];
+        loopAudioSource = temp[1];
+    }
+
+    void OnDestroy()
+    {
+        loopAudioSource.Stop();
+        Instance = null;
+    }
+
+    public void Play(string clipName)
+    {
+        loopAudioSource.clip = GetAudioClip(clipName);
+        loopAudioSource.Play();
+    }
+
+    public void PlayOneShot(string clipName, float volume = 1.0f)
+    {
+        audioSource.PlayOneShot(GetAudioClip(clipName), volume);
+    }
+
+    AudioClip GetAudioClip(string clipName)
+    {
+        AudioClip clip = clipList.Find(n => n.name == clipName);
+
+        if (clip != null) return clip;
+
+        clip = Resources.Load<AudioClip>(clipName);
+
+        if (clip != null)
+        {
+            clipList.Add(clip);
+            return clip;
+        }
+
+        clip = Resources.Load<AudioClip>("Audios/" + clipName);
+
+        if (clip != null)
+        {
+            clipList.Add(clip);
+            return clip;
+        }
+
+        return null;
+    }
+}
