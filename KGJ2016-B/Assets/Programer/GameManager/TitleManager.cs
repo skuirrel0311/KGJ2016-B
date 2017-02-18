68f43b833a9970ead14ca2bc2a679db97bff1e0d
sakaki-shunsuke <g1527035@st.t-kougei.ac.jp>
2017-02-18 12:41:36 +0900

    シーン遷移を作成

---
+++ KGJ2016-B/Assets/Programer/GameManager/TitleManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class TitleManager : BaseManager<TitleManager>
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
