a3db175f56575d54ad07e6bfad915cccbdd33001
GCR-B06\ladmin <g1427050@st.t-kougei.ac.jp>
2017-02-18 18:52:23 +0900

    触手追加

---
+++ KGJ2016-B/Assets/Programer/Enemy/Scripts/TentacleContoller.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TentacleContoller : MonoBehaviour
+{
+    [SerializeField]
+    GameObject tip = null;
+
+    GameObject player;
+
+    [SerializeField]
+    float powar = 1000.0f;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        StartCoroutine(StartUp());
+    }
+    void Update()
+    {
+    }
+
+    IEnumerator StartUp()
+    {
+        float t = 0.0f;
+        Vector3 startPosition = transform.position;
+        Vector3 targetPosition = startPosition;
+        targetPosition.y = 1.0f;
+
+        while(true)
+        {
+            t += Time.deltaTime;
+
+            transform.position = Vector3.Lerp(startPosition, targetPosition, t / 3.0f);
+            if (t > 3.0f) break;
+            yield return null;
+        }
+
+        //倒したい方向を調べる
+        Vector3 targetDirection;
+        if (player != null)
+            targetDirection = Vector3.Normalize(player.transform.position - tip.transform.position);
+        else
+            targetDirection = Vector3.Normalize(Vector3.zero - tip.transform.position);
+
+        tip.GetComponent<Rigidbody>().AddForce(targetDirection * Time.deltaTime * powar);
+    }
+}
---
+++ KGJ2016-B/Assets/Programer/Enemy/Scripts/TentacleManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TentacleManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject enemyPrefab = null;
+
+    [SerializeField]
+    List<Transform> startPositions = new List<Transform>();
+
+    void Start()
+    {
+        startPositions.AddRange(GetComponentsInChildren<Transform>());
+        Transform temp = startPositions.Find(n => n.Equals(transform));
+        startPositions.Remove(temp);
+
+        StartCoroutine(EnemyCreator());
+    }
+    
+    IEnumerator EnemyCreator()
+    {
+        WaitForSeconds wait = new WaitForSeconds(5.0f);
+        yield return new WaitForSeconds(1.0f);
+        int index = Random.Range(0, startPositions.Count);
+        Instantiate(enemyPrefab, startPositions[index].position, startPositions[index].rotation);
+        while (true)
+        {
+            yield return wait;
+            index = Random.Range(0, startPositions.Count);
+            Instantiate(enemyPrefab, startPositions[index].position, startPositions[index].rotation);
+        }
+    }
+}
